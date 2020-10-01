bf7707e231dd2b9530fac85ef4ac8fc119a81ff8
Alex Withington-Smith <alex@awithsmith.com>
2020-10-01 11:51:57 +0100

    added seperate logic for uppercase

--- caesar.rb
+++ caesar.rb
@@ -2,9 +2,13 @@ def caesar_cipher(string, shift)
     # Iterate over all chars in string
     string.split("").map do |char|
         # Check if character
-        if char.match(/[a-zA-Z]/)
+        if char.match(/[A-Z]/)
             # Add shift if so
-            (char.ord() + shift).chr 
+            # work out alphabet position in normal terms
+            reg_alphabet = char.ord - 64
+            # add the shift with mod for overspill, convert back to ascii
+            new_value = (char.ord + shift) / 26 + 64
+            char = new_value.chr
         else
             # Use just the input if not
             char
