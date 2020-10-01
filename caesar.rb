9a3920c307c015e0f9da569e857d8d8cc086e8f3
Alex Withington-Smith <alex@awithsmith.com>
2020-10-01 10:45:58 +0100

    Maps char values and adds shift to them, converts back. Needs mod to stop weird characters, and exception for spaces/punctation

--- caesar.rb
+++ caesar.rb
@@ -1,3 +1,6 @@
 def caesar_cipher(string, shift)
-    return string
+    string.split("").map { |char| p (char.ord() + shift).chr } 
 end
+
+# test - expecting "Bmfy f xywnsl!"
+p caesar_cipher("What a string!", 5)
